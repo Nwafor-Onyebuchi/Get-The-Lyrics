c905dd9f93fcbe6f39de48452b8383f528a65c33
Nwafor Onyebuchi <buchilazarus4@gmail.com>
2019-08-08 08:22:54 +0100

    Updates

---
+++ precache-manifest.451cffa758a23019118941727b14bbeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ad783b5ae2b0149aad19693e764b86",
+    "url": "/Nwafor-Onyebuchi/Get-The-Lyrics.git/index.html"
+  },
+  {
+    "revision": "8b55018987ed6d2aba62",
+    "url": "/Nwafor-Onyebuchi/Get-The-Lyrics.git/static/css/main.8c2a8f52.chunk.css"
+  },
+  {
+    "revision": "001e0eca61cfd9b4870b",
+    "url": "/Nwafor-Onyebuchi/Get-The-Lyrics.git/static/js/2.1a3a55ec.chunk.js"
+  },
+  {
+    "revision": "8b55018987ed6d2aba62",
+    "url": "/Nwafor-Onyebuchi/Get-The-Lyrics.git/static/js/main.3508fd18.chunk.js"
+  },
+  {
+    "revision": "ba04c9f14ddc7ab3517d",
+    "url": "/Nwafor-Onyebuchi/Get-The-Lyrics.git/static/js/runtime~main.28142f96.js"
+  }
+]);
